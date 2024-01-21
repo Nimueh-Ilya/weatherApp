2a8df21b34f4b5318b255f9b1907448d19d0d54a
Nimueh <139075582+Nimueh-Ilya@users.noreply.github.com>
2024-01-21 19:04:59 +0100

    add json parse

--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
-const apiKeyParameter = "?key=80f5580617a24725a91193618241401";
-const requestURL = "http://api.weatherapi.com/v1";
-const currentWeatherURL = "/current.json";
-const forecastWeatherURL = "/forecast.json";
-
 async function getWeather() {
+  const userLocationInputElement = document.querySelector("element");
+  // const userLocationInput = userLocationInputElement.value;
+  const apiKeyParameter = "?key=80f5580617a24725a91193618241401";
+  const requestURL = "http://api.weatherapi.com/v1";
+  const forecastWeatherURL = "/forecast.json";
   response = await fetch(
-    `${requestURL + forecastWeatherURL + apiKeyParameter}&q=london&days=3`,
+    `${requestURL + forecastWeatherURL + apiKeyParameter}&q=london`,
     {
       mode: "cors",
     }
   );
   answer = await response.json();
   console.log(answer);
+  return answer;
+}
+function processWeather(jsonObject) {
+  const currentDay = jsonObject.forecast.forecastday[0];
+  const dayObject = {
+    location: jsonObject.loction.name,
+    firstHour: {
+      temp: currentDay.hour[7].temp_c,
+      windspeed: currentDay.hour[7].wind_kph,
+      humidity: currentDay.hour[7].humidity,
+      condition_icon: currentDay.hour[7].condition.icon,
+    },
+    secondHour: {
+      temp: currentDay.hour[12].temp_c,
+      windspeed: currentDay.hour[12].wind_kph,
+      humidity: currentDay.hour[12].humidity,
+      condition_icon: currentDay.hour[12].condition.icon,
+    },
+    thirdHour: {
+      temp: currentDay.hour[19].temp_c,
+      windspeed: currentDay.hour[19].wind_kph,
+      humidity: currentDay.hour[19].humidity,
+      condition_icon: currentDay.hour[19].condition.icon,
+    },
+    fourthHour: {
+      temp: currentDay.hour[21].temp_c,
+      windspeed: currentDay.hour[21].wind_kph,
+      humidity: currentDay.hour[21].humidity,
+      condition_icon: currentDay.hour[21].condition.icon,
+    },
+    averageTemp: currentDay.day.avgtemp_c,
+    averageHumidity: currentDay.day.avghumidity,
+    maxWind: currentDay.day.maxwind_kph,
+  };
 }
 getWeather();
