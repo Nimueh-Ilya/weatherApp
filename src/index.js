732e16033a53ed7a70113164df76f28650c9c585
Nimueh <139075582+Nimueh-Ilya@users.noreply.github.com>
2024-01-22 19:55:53 +0100

    Add html elements to hold weather info

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+const searchButton = document.querySelector("button");
+const userInputElement = document.querySelector(".user-input");
+let userInput;
 async function getWeather() {
-  const userLocationInputElement = document.querySelector("element");
-  // const userLocationInput = userLocationInputElement.value;
   const apiKeyParameter = "?key=80f5580617a24725a91193618241401";
   const requestURL = "http://api.weatherapi.com/v1";
   const forecastWeatherURL = "/forecast.json";
   response = await fetch(
-    `${requestURL + forecastWeatherURL + apiKeyParameter}&q=london`,
+    `${requestURL + forecastWeatherURL + apiKeyParameter}&q=${userInput}`,
     {
       mode: "cors",
     }
@@ -14,10 +15,14 @@ async function getWeather() {
   return answer;
 }
 async function processWeather(jsonObject) {
+  userInput = userInputElement.value;
   const mainObject = await jsonObject();
   const currentDay = mainObject.forecast.forecastday[0];
   const dayObject = {
     location: mainObject.location.name,
+    averageTemp: currentDay.day.avgtemp_c,
+    averageHumidity: currentDay.day.avghumidity,
+    maxWind: currentDay.day.maxwind_kph,
     firstHour: {
       temp: currentDay.hour[7].temp_c,
       windspeed: currentDay.hour[7].wind_kph,
@@ -42,9 +47,19 @@ async function processWeather(jsonObject) {
       humidity: currentDay.hour[21].humidity,
       condition_icon: currentDay.hour[21].condition.icon,
     },
-    averageTemp: currentDay.day.avgtemp_c,
-    averageHumidity: currentDay.day.avghumidity,
-    maxWind: currentDay.day.maxwind_kph,
   };
+
+  return dayObject;
 }
-processWeather(getWeather);
+searchButton.addEventListener("click", () => {
+  const userInput = userInputElement.value;
+  console.log(userInput);
+  if (!userInput) {
+    return;
+  } else {
+    (async () => {
+      const result = await processWeather(getWeather);
+      console.log(result);
+    })();
+  }
+});
