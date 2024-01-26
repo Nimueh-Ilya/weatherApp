e2938d2c2539999da879651ef8270fb45e780236
Nimueh <139075582+Nimueh-Ilya@users.noreply.github.com>
2024-01-26 15:25:01 +0100

    Testing https

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const searchButton = document.querySelector("button");
 const userInputElement = document.querySelector(".user-input");
 const apiKeyParameter = "?key=80f5580617a24725a91193618241401";
-const requestURL = "http://api.weatherapi.com/v1";
+const requestURL = "https://api.weatherapi.com/v1";
 const forecastWeatherURL = "/forecast.json";
 let userInput;
 async function getWeather() {
